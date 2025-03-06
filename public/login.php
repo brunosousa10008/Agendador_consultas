7feabb857077170e07f052ec8ff1a92eef8f1ca3
brunosousa10008 <brunoteixeira10008@gmail.com>
2025-03-06 08:45:18 -0300

    inicio página de login

--- public/login.php
+++ public/login.php
@@ -3,14 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=], initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/login.css">
     <title>Document</title>
 </head>
 <body>
-    <div class="head">
-        <h1>Clínica MedPrime</h1>
-    </div>
-    <div class="body">
-        
-    </div>
+    <main>
+        <div class="login-submit">
+            <div class="head">
+                <h1>Clínica MedPrime</h1>
+            </div>
+            <div class="body">
+                <div>
+                    <label for="user">Login</label>
+                    <input type="text" name="user">
+                </div>
+                <div>
+                    <label for="pass">Senha</label>
+                    <input type="password" name="pass">
+                </div>
+                <div>
+                    <button>Entrar</button>
+                </div>
+            </div>
+        </div>
+        <div class="login-wallpaper">
+        </div>
+    </main>
 </body>
 </html>
